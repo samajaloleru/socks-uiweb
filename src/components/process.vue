fe6bd3266a98bbc580fc77568b5a7ea841b67439
samajaloleru <samsonajaloleru@gmail.com>
2019-10-08 12:58:30 +0100

    Cleaned up our process page

--- src/components/process.vue
+++ src/components/process.vue
@@ -17,139 +17,167 @@
         </div>
 
         
-        <div class=" w-100 bt ">
+        <div class="cf w-100 bt ">
             <div class="mw8 center v-mid pt5">
                 <div class="w-50-l w-100 fl pa2 pa0-l">
                     <img src="@/assets/img/bg1.jpg" class="dib  " >          
                 </div>
                 <div class="w-50-l pl4-l pa2 pa0-l v-mid mid-gray w-100 fl">
-                    <div class="h3 w-100"></div>
-                    <div class="tc">
-                        <div class="tc">
-                            <router-link to="story" class="f4 link w4 h4 pt4 mb2 dib tc shadow-1 fw6 tracked ttu br-100 bg-dark-gray washed-yellow ba b--white-40">
-                                Step <br/> <span class="f2">1</span>
+                    <div class="dtc pt3">
+                        <div class="tr w-100 f2">
+                            Step <br/> <span class="f-headline">1</span>
+                        </div>
+
+                         <div class="tr ">
+                            <div class="f3 navy measure">Choose your product type, category and sock.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cf w-100 bt ">
+            <div class="mw8 center v-mid pt5">
+                <div class="w-50-l pa2 pa0-l v-mid mid-gray w-100 fl">
+                    <div class="dtc pt3">
+                        <div class="tl w-100 f2">
+                            Step <br/> <span class="f-headline">2</span>
+                        </div>
+
+                         <div class="tr ">
+                            <div class="f3 navy measure">Customize your sock colors and graphics and even upload your own custom logo.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-50-l w-100 fl pl4-l pa2 pa0-l">
+                    <img src="@/assets/img/bg1.jpg" class="dib  " >          
+                </div>
+            </div>
+        </div>
+        <div class="cf w-100 bt bg-gray ">
+            <div class="mw8 center v-mid pt5">
+                <div class="w-50-l w-100 fl pa2 pa0-l">
+                    <img src="@/assets/img/bg1.jpg" class="dib  " >          
+                </div>
+                <div class="w-50-l pl4-l pa2 pa0-l v-mid w-100 fl">
+                    <div class="dtc pt3">
+                        <div class="tr white w-100 f2">
+                            Step <br/> <span class="f-headline">3</span>
+                        </div>
+
+                         <div class="tr ">
+                            <div class="f3 navy measure">Finalize your order and complete checkout.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cf w-100 bt ">
+            <div class="mw8 center v-mid pt5">
+                <div class="w-50-l pa2 pa0-l v-mid mid-gray w-100 fl">
+                    <div class="dtc pt3">
+                        <div class="tl w-100 f2">
+                            Step <br/> <span class="f-headline">4</span>
+                        </div>
+
+                         <div class="tr ">
+                            <div class="f3 navy measure">Receive your art proof within 48 hours and approve or revise your artwork.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-50-l w-100 fl pl4-l pa2 pa0-l">
+                    <img src="@/assets/img/bg1.jpg" class="dib  " >          
+                </div>
+            </div>
+        </div>
+        <div class="cf w-100 bt ">
+            <div class="mw8 center v-mid pt5">
+                <div class="w-50-l w-100 fl pa2 pa0-l">
+                    <img src="@/assets/img/bg1.jpg" class="dib  " >          
+                </div>
+                <div class="w-50-l pl4-l pa2 pa0-l v-mid mid-gray w-100 fl">
+                    <div class="dtc pt3">
+                        <div class="tr w-100 f2">
+                            Step <br/> <span class="f-headline">5</span>
+                        </div>
+
+                         <div class="tr ">
+                            <div class="f3 navy measure">Receive and enjoy your awesome custom socks!</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="dt w-100 center pa4 bg-dark-gray">
+            <div class="dtc v-mid">
+                <div class="cf center mw8">
+                    <div class="f1-l f2-m f3 white pv2 b tc">You look like you're ready.</div>
+                    <div class="f4-l f5 f6 white-70 tc">{CLICK ONE OF THE LINKS BELOW TO START YOUR EPIC CUSTOM SOCK JOURNEY}</div>
+                    <div class="fl w-100">
+                        <div class="fl-ns w-third-ns w-100 pa2 tc">
+                            <article class="bg-washed-yellow ba dark-gray shadow-2 b--black-20 mv4 mw9 center">
+                                <div class="pa2 pb3-ns">
+                                    <p class="f3 f6-m lh-copy measure mt2">Shop Custom Athletic Socks</p>
+                                    <div class>
+                                        <router-link to="story" class="f5 link ph4 ph2-m pv1-m pv3 mb2 dib shadow-1 fw6 tracked ttu br2 bg-mid-gray white-80 ba b--white-40">
+                                            CHOOSE
+                                        </router-link>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+
+                        <div class="fl-ns w-third-ns w-100 pa2 tc">
+                            <article class="ba bg-washed-yellow dark-gray shadow-2 b--black-20 mv4 mw9 center">
+                                <div class="pa2 pb3-ns">
+                                    <p class="f3 f6-m lh-copy measure mt2">Shop Custom Dress Socks</p>
+                                    <div class>
+                                        <router-link to="story" class="f5 link ph4 ph2-m pv1-m pv3 mb2 dib shadow-1 fw6 tracked ttu br2 bg-mid-gray white-80 ba b--white-40">
+                                            CHOOSE
+                                        </router-link>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+
+                        <div class="fl-ns w-third-ns w-100 pa2 tc">
+                            <article class="ba bg-washed-yellow dark-gray shadow-2 b--black-20 mv4 mw9 center">
+                                <div class="pa2 pb3-ns">
+                                    <p class="f3 f6-m lh-copy measure mt2">Shop Custom Marketing Socks</p>
+                                    <div class>
+                                        <router-link to="story" class="f5 link ph4 ph2-m pv1-m pv3 mb2 dib shadow-1 fw6 tracked ttu br2 bg-mid-gray white-80 ba b--white-40">
+                                            CHOOSE
+                                        </router-link>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-white pa4-l pa2 pv3 cf w-100">
+            <div class="cf mw8 center">
+                <div class="fl tc w-50-l w-30-m w-100">
+                    <div class="f2-l pt2-m f4 fw6 navy measure">Still have questions?</div>
+                </div>
+                <div class="fl w-50-l w-70-m w-100">
+                    <div class="cf pt2 pt0-ns">
+                        <div class="fl pl2-m w-40">
+                            <router-link to="story" class="f6 link ph2 tc w-100 pv3 dib shadow-1 fw6 tracked ttu br1 bg-mid-gray white ba b--white-40">
+                                READ OUR FAQ
                             </router-link>
                         </div>
-                        <div class="f3 navy">Choose your product type, category and sock.</div>
-                    </div>
-                    
-                </div>
-                
-            </div>
-        </div>
-
-        <div class="dt w-100 center pa4 pa5-m pa6-l bg-near-white">
-            <div class="dtc v-mid">
-                <div class="w-50-l w-100 tc fl mid-gray ph4">
-                    <div class="f2 ttu orange fw6">Our Sock Tech</div>
-                    <div class="pb4 navy">
-                        <h1 class="b f4">NO-SLIP CUFF</h1>
-                        <p class="tc center measure-narrow">
-                            Socks guaranteed to stay put all day under the toughest circumstances. 
-                        </p>
-                    </div>
-                    <div class="pb4 navy">
-                        <h1 class="b f4">HIGH QUALITY COMBED COTTON</h1>
-                        <p class="tc center measure-narrow">
-                            Constructed from 70% combed cotton for an ultra-soft yet durable fit every time.
-                        </p>
-                    </div>
-                    <div class="pb4 navy">
-                        <h1 class="b f4">200 NEEDLE CONSTRUCTION</h1>
-                        <p class="tc center measure-narrow">
-                            200 needle machine yield brighter, softer, more detailed, and longer lasting socks.
-                        </p>
-                    </div>
-                    <div class="pb4 navy">
-                        <h1 class="b f4">REINFORCED HEEL AND TOE</h1>
-                        <p class="tc center measure-narrow">
-                            Strong fibers to give you the durability you need without the added bulk.
-                        </p>
-                    </div>
-                </div>
-                <div class="w-50-l w-100 fl">
-                    <div class="mw6">
-                        <div class="aspect-ratio aspect-ratio--16x9">
-                            <div class="aspect-ratio--object cover" :style="{backgroundImage:'url('+require('@/assets/img/background2.png')+')'}"></div>
-                        </div>
-                        <div class="aspect-ratio aspect-ratio--16x9">
-                            <div class="aspect-ratio--object cover" :style="{backgroundImage:'url('+require('@/assets/img/background4.png')+')'}"></div>
+                        <div class="fl w-20-ns w-10 tc f3 pt2">OR</div>
+                        <div class="fl w-40-ns w-50">
+                            <div class="f6 ph2 pv3 dib shadow-1 w-100 tc fw6 tracked ttu br1 bg-mid-gray white ba b--white-40">
+                                Call (888) 557-9185
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class=" w-100 center pa4 pa5-m pa6-l">
-            <h1 class="b ttu navy tc f1">Our Social</h1>
-            <div class="w-80 bw2 center bb b--orange"></div>
-            
-            <h1 class="ttu tc mid-gray pt4 f4"># F i T S O C K</h1>
-            
-            <div class="pa2 tc">
-                <i class="mr2 mr3-l f2 orange fab fa-facebook-f"></i> 
-                <i class="mr2 mr3-l f2 orange fab fa-twitter"></i> 
-                <i class="mr2 mr3-l f2 orange fab fa-linkedin-in"></i> 
-                <i class="f2 orange fab fa-instagram"></i>
-            </div>
-            
-            <main class="cf mt4 w-100">
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0006.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0002.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0003.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0004.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0007.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0008.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0008.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                <div class="fl w-50 w-third-m w-25-ns">
-                    <div class="aspect-ratio aspect-ratio--16x9">
-                    <div style="background-image:url(http://mrmrs.github.io/images/0008.jpg);"
-                        class="bg-center cover aspect-ratio--object"></div>
-                    </div>
-                </div>
-                
-            </main>
-        </div>
-        <p class="tc center f4 orange b">
-            Join Us on Instagram
-        </p>
-        
-      
     <sitefooter/>
   </section>
 </template>
