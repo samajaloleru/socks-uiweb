39fcb287969dbaa50f1e2d27aa0b60b15cee3efd
samajaloleru <samsonajaloleru@gmail.com>
2019-10-22 12:51:17 +0100

    Cleaned up footer page

--- src/components/sitefooter.vue
+++ src/components/sitefooter.vue
@@ -1,85 +1,81 @@
 <template>
-    <footer class="fl pa4 bg-mid-gray white w-100">
-        <div class="pb3 mw9 center">
-            <div class="fl pb5 w-100 ">
-                <div class="fl w-20-l w-100 tc pt3 ">
-                    <p class="ttu dark-gray b f2">follow</p>
+    <footer class="dt v-mid pa4 bg-navy white vh-50 w-100">
+        <div class="cf mw9 mb5 ph2 center">
+            <div class="dt bb pb3 w-100">
+                <div class="dtc v-mid">
+                    <img src="@/assets/img/logo.png"  class="w5">
+                </div>
+                <div class="dtc fr v-mid">
+                    <img src="@/assets/img/nigeria.png"  class="w3">
+                </div>
+            </div>
+            <div class="fl w-100 pv4">
+                <div class="fl w-50">
+                    <div class="fl w-80 pr4">
+                        <div class="fl w-60">
+                            <div class="center pv2">
+                                <div class="cf bn ma0 pa0">
+                                    <legend class="pa0 f5 f4-ns mb3 white">BE THE FIRST TO KNOW</legend>
+                                    <div class="cf ba">
+                                        <input class="f6 f5-l fl input-reset signin bg-transparent pa2 lh-solid w-100 w-75-m w-80-l b--white br--right-ns" placeholder="Email Address" type="text" name="email-address" value="" id="email-address">
+                                        <input class="f6 f5-l button-reset fl pv2 tc bn hover-bg-black white pointer w-100 w-25-m b--orange bw2 w-20-l br2-ns br--right-ns" type="submit" value="Join">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="fl w-40">
+                            <div class="cf pa2 pl3">
+                                <legend class="pa0 f5 f4-ns mb3 white">STORE LOCATOR</legend>
+                                <div class="cf">
+                                    <div class="ba tc pa2">
+                                        Find Store
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="fl w-50">
+                    <article class="tl f7 fr f5 dib-ns w-40 silver">
+                        <div class="f4 glow dark-gray pb3 b">CONTACT US</div>
+                        <div class="pv1">Email <span class="orange">info@stance.com</span></div>
+                        <div class="pv1">Call <span class="orange">(888) 391-9020</span></div>
+                        <div class="pv1 mb3">Text <span class="orange">(949) 391-4272</span></div>
+                        <div class="pv1">Mon-Th <span class="orange">7am-5pm PST</span></div>
+                        <div class="pv1">Fri <span class="orange">7am-4pm PST</span></div>                    
+                    </article>
+                </div>
+            </div>
+            <div class="fl w-100">
+                <div class="fl w-40">
                     <div class="pa2">
-                        <i class="mr2 mr3-l f2 white fab fa-facebook-f"></i> 
-                        <i class="mr2 mr3-l f2 white fab fa-twitter"></i> 
-                        <i class="mr2 mr3-l f2 white fab fa-linkedin-in"></i> 
-                        <i class="f2 white fab fa-instagram"></i>
+                        <i class="mr2 mr3-l f4 white fab fa-facebook-f"></i> 
+                        <i class="mr2 mr3-l f4 white fab fa-twitter"></i> 
+                        <i class="mr2 mr3-l f4 white fab fa-linkedin-in"></i> 
+                        <i class="f4 white fab fa-instagram"></i>
                     </div>
-                    <div class="fl pa3 tc w-100">
-                        <img src="@/assets/img/footer.png"  class="h5">
+                </div>
+                <div class="fl w-60">
+                    <div class="tr pa2">
+                        <a href="/language/" title="Language" class="f5 dib ph2 link white dim">Our Story</a>
+                        <a href="/terms/"    title="Terms" class="f5 dib ph2 link white dim">Careers</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">The Store</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">Loyalty</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">Recycle</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">Help</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">Shipping</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">Returns</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">Gift Card</a>
+                        <a href="/privacy/"  title="Privacy" class="f5 dib ph2 link white dim">Contact Us</a>
                     </div>
-
-                
-                </div>
-                <div class="fl w-20-l w-50 pa3 ">
-                    <article class="tl f5 dib-ns w-100">
-                        <h4 class="f3 glow dark-gray b">SHOP</h4>
-                        <div class="pb1">SUBSCRIPTION</div>
-                        <div class="pv1">GIFT SUBSCRIPTION </div>
-                        <div class="pv1">BULK SUBSCRIPTION </div>
-                        <div class="pv1">SHARK WEEK SOCKS</div>
-                        <div class="pv1">CUSTOM SOCKS </div>                    
-                    </article>
-                    
-                </div>
-                <div class="fl w-20-l w-50 pa3">
-                    <article class="tl f5 dib-ns w-100">
-                        <h4 class="f3 glow dark-gray b">USEFUL STUFF</h4>
-                        <div class="pb1">CONTACT US</div>
-                        <div class="pv1">MY ACCOUNT</div>
-                        <div class="pv1">FAQS</div>
-                        <div class="pv1">OUR STORY</div>
-                        <div class="pv1">OUR SOCKS</div>
-                        <div class="pv1">OUR BLOG </div>                    
-                        <div class="pv1">PRINTABLE GIFT RECEIPT</div>                    
-                    </article>
-                </div>
-                <div class="fl w-40-l w-100 pa3">
-                    <div class="w-100" >
-                        <h4 class="f3 glow dark-gray b">JOIN THE FAM</h4>
-                        <p class="f5 white mb2 mt0">
-                            Sign up to get the latest on sales, new releases and more …
-                        </p>
-                        <div class="w-100 fl mt3">
-                            <div class="fl w-50-ns w-50 ">
-                                <input class="fl w-100 f4 input-reset bn black bg-white-70 pa3 lh-solid" type="text" placeholder="First name"  v-model="contact.firstname" />
-                            </div>
-
-                            <div class="fl w-40-ns w-50 pl3">
-                                <input class="fl w-100 f4 input-reset bn black bg-white-70 pa3 lh-solid" type="text" placeholder="Last name"  v-model="contact.lastname" />
-                            </div>
-                        </div>
-                        <div class="w-100 fl mt3">
-                            <div class="fl w-70-ns w-100">
-                                <input class="fl w-100 f4 input-reset bn black bg-white-70 pa3 lh-solid" type="email" placeholder="Enter Your Email Address"  v-model="contact.email" />
-                            </div>
-                        </div>
-                        <div class="fl w-100 pa2 mt3">
-                            <span @click="submitContact" class="f6 button-reset fw6 fl pa3 tc bn bg-moon-gray ttu tracked br3 hover-bg-dark-green white pointer">SIGN UP</span>
-                        </div>
-                        <p class="mt7">
-                            © {{date}} FiT Socks &nbsp;
-                        </p>
-                    </div>
-                </div>           
-            </div>
-
-            
-            <div class="pa3 tc w-100 center">
-                <div class="fl tc pa3 bg-dark-gray washed-yellow w-100 f4 b tracked">
-                    Copyright © {{date}} FiT Socks &nbsp;  <span class="dn dib-l white">|</span>
-                    <div class="w-100 cf dn-l pv1"></div> 
-                    <router-link to="terms" class="white link ph1">Terms and Conditions</router-link> -
-                    <router-link to="privacy" class="white link ph1">Privacy Policy</router-link> 
                 </div>
             </div>
-        </div>
-        
+            <div class="pa3 cf tc w-100 center">
+                <div class="fl tr washed-yellow w-100 f5 b tracked">
+                    © {{date}} FiT Socks. All Rights Reserved.
+                </div>
+            </div>
+        </div>      
     </footer>
 </template>
 
